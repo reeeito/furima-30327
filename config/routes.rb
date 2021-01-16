8d430f69a75fa212d56cc4cb17d299b607f44fe2
reeeito <barca.0716@i.softbank.jp>
2021-01-16 17:44:52 +0900

    view修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root  'users#index'  #この1行を追加
+  root to: 'items#index'
 end
