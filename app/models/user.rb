21018d797f9422024d294779ef29d9beae9013a0
reeeito <barca.0716@i.softbank.jp>
2021-01-24 21:46:12 +0900

    バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :lastname, presence: true
+  validates :firstname, presence: true
+  validates :lastname_kana, presence: true
+  validates :firstname_kana, presence: true
+  validates :birthday, presence: true
+
+  #has_many :products
+  #has_many :purchase_histries
 end
