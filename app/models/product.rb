d6800f28a662f76599ef10423b714eee3b2e09f4
reeeito <barca.0716@i.softbank.jp>
2021-01-31 16:22:07 +0900

    validation実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
     belongs_to :user
     #has_one :purchace_history
+    with_options presence: true do
+        validates :image
+        validates :productname
+        validates :explanation
+        validates :category_id
+        validates :state_id
+        validates :delivery_fee_id
+        validates :area_id
+        validates :delivery_time_id
+        validates :price,numerically:{greater_than:300,less_than:9999999},format:{ with: /\A[0-9]+\z/ }
+    end
+    validates
 end
