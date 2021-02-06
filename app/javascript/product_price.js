90d6273434c25c89dcd0a92fb282d756bcc6b89a
reeeito <barca.0716@i.softbank.jp>
2021-02-07 08:35:06 +0900

    金額機能実装

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -1,3 +1,14 @@
 window.addEventListener('load', () => {
-    console.log("OK");
-  });
+
+const priceInput = document.getElementById("item-price");
+const addTaxDom = document.getElementById('add-tax-price');
+const profitDom = document.getElementById('profit');
+
+priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    profitDom.innerHTML = Math.floor(inputValue - inputValue * 0.1);
+    console.log(inputValue);
+})   
+
+});
