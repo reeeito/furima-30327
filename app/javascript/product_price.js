86282d9cb565ac5cdc5f1655253ab9f18b34beb9
reeeito <barca.0716@i.softbank.jp>
2021-02-08 21:39:09 +0900

    デバック用の記述削除

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -8,6 +8,5 @@ priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
     profitDom.innerHTML = Math.floor(inputValue - inputValue * 0.1);
-    console.log(inputValue);
 })   
 });
