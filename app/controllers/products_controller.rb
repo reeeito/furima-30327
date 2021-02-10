5f7e96bb64480c0d82ce44e028e96d9165fa92c6
reeeito <barca.0716@i.softbank.jp>
2021-02-10 11:19:59 +0900

    昇順機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,9 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   def index
     @products = Product.all
+    @products = Product.all.order("created_at DESC")
   end
 
   def new
