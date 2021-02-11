42542eeba39ee9c8d2d6895ee4ebec8ec56378bc
reeeito <barca.0716@i.softbank.jp>
2021-02-11 18:22:36 +0900

    保存機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,12 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  belongs_to :delivery_fee
-  belongs_to :state
-  belongs_to :category
-  belongs_to :delivery
-  belongs_to :area
-  belongs_to :delivery_time
+  belongs_to :delivery_fee,optional: true
+  belongs_to :state,optional: true
+  belongs_to :category,optional: true
+  belongs_to :delivery,optional: true
+  belongs_to :area,optional: true
+  belongs_to :delivery_time,optional: true
   has_one_attached :image
   # has_one :purchace_history
   with_options presence: true do
