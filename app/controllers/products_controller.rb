99a9b9d1a48fdc377a66380e3d2490f296ee4a9f
reeeito <barca.0716@i.softbank.jp>
2021-02-11 09:12:01 +0900

    商品表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   #before_action :authenticate_user!
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def index
     @products = Product.all.order("created_at DESC")
   end
