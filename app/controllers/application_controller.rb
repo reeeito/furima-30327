f1533d75bfdc4b7e108671a3617745144901f78a
reeeito <barca.0716@i.softbank.jp>
2021-02-27 17:59:12 +0900

    BASIC認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -7,4 +8,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: %i[nickname lastname firstname firstname_kana lastname_kana birthday])
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'name' && password == '2222'
+    end
+  end
+
 end
